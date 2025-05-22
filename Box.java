4c6fb6c7e9eaa6bb948b57ebcb344c6a95e21015
pujitha-reddy-gth <pujithathammaneni@gmail.com>
2025-05-22 23:09:56 +0530

    Add files via upload

---
+++ Box.java
@@ -0,0 +1,18 @@
+public class Box{
+	int length;
+	public Box(int length){
+		this.length=length;
+	}
+	public Box setLength(int length){
+		this.length=length;
+		return this;
+	}
+	public void display(){
+		System.out.println("Box length:"+length);
+	}
+	public static void main(String[] args){
+		Box box=new Box(10);
+		box.setLength(19).display();
+	}
+	
+}
